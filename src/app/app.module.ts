aa9b5cc30d70690ed0a2d6cc80dd6edc2c77a889
Thibault-Renouat <trenouat@stagiaire-humanbooster.com>
2020-07-02 09:15:53 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComputersComponent } from './components/computers/computers.component';
+import { ComputersDetailComponent } from './components/computers-detail/computers-detail.component';
+import { AddComputerComponent } from './components/add-computer/add-computer.component';
+import { EditComputerComponent } from './components/edit-computer/edit-computer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComputersComponent,
+    ComputersDetailComponent,
+    AddComputerComponent,
+    EditComputerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/computers-detail/computers-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-computers-detail',
+  templateUrl: './computers-detail.component.html',
+  styleUrls: ['./computers-detail.component.css']
+})
+export class ComputersDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
